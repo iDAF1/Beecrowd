7689aa327dc9a9ac19ce7b6aa5bf24d9c2f65d80
iDAF1 <softengine.daniel.sf@gmail.com>
2025-02-09 13:17:45 -0300

    Add files via upload

---
+++ beginner-py/1011.py
@@ -0,0 +1,7 @@
+#! /bin/pydoc3.12
+# -*- coding: utf-8 -*-
+r     = float(input())
+pi    = 3.14159
+value = ((4/3.0) * pi * r**3)
+print(f'VOLUME = {value:.3f}')
+
---
+++ beginner-py/1012.py
@@ -0,0 +1,11 @@
+#! /bin/pydoc3.12
+# -*- coding: utf-8 -*-
+a, b, c = map(float,input().split())
+output  = [
+    f'TRIANGULO: {((a * c) / 2):.3f}',
+    f'CIRCULO: {(c**2 * 3.14159):.3f}',
+    f'TRAPEZIO: { (((a + b) * c)/ 2):.3f}',
+    f'QUADRADO: {(b**2):.3f}',
+    f'RETANGULO: {(a * b):.3f}',
+] 
+for i in output: print(i)
