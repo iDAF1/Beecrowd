009ea67fa62061eb9797a69155dda9dd20b2c28b
iDAF1 <softengine.daniel.sf@gmail.com>
2025-02-12 09:54:48 -0300

    Update 1037.py

--- beginner-py/1037.py
+++ beginner-py/1037.py
@@ -1,19 +1,14 @@
 #! /bin/python3.12
 # -*- coding: utf-8 -*-
 x = float(input())
-
 if not (x < 0 or x > 100):
     if x <= 25.0:
         print('Intervalo [0,25]')
-
     elif 25.0 < x <= 50.0:
         print('Intervalo (25,50]')
-
     elif 50.0 < x <= 75.0:
         print('Intervalo (50,75]')
-
     elif 75.0 < x <= 100.0:
         print('Intervalo (75,100]')
-
 else:
     print('Fora de intervalo')
