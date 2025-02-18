851e0331ecff9200b57733f24255904662af9939
iDAF1 <softengine.daniel.sf@gmail.com>
2025-02-18 10:48:53 -0300

    Add files via upload

--- list-all-py/1016.py
+++ list-all-py/1016.py
@@ -1,6 +1,5 @@
-#! /usr/bin/python3.12
+#! /bin/python3.12
 # -*- coding: utf-8 -*-
-n =  int(input())
-if 5 < n < 2000:
-    for i in range(2,n + 1,2):
-        print(f'{i}^2 = {i**2}')
+distance = int(input())
+time_in_minutes = distance / 0.5
+print(f"{int(time_in_minutes)} minutos")
